6c5060fe0b4c0ee868ac2de2952e6bb7b77e5710
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-02-24 23:19:32 -0800

    Added code to read the Spark plan to act on from disk.

--- src/gateway/converter/spark_to_substrait_test.py
+++ src/gateway/converter/spark_to_substrait_test.py
@@ -3,6 +3,7 @@ import pytest
 from pathlib import Path
 
 from gateway.converter.spark_to_substrait import SparkSubstraitConverter
+import spark.connect.base_pb2 as pb2
 
 
 test_case_directory = Path(os.path.dirname(os.path.realpath(__file__))) / 'data'
@@ -17,7 +18,12 @@ test_case_names = [os.path.basename(p).removesuffix('.spark') for p in test_case
     ids=test_case_names,
 )
 def test_func(request: pytest.FixtureRequest):
-    spark_plan = request.param
+    with open(request.param) as f:
+        plan_protobuf = f.read()
+    spark_plan = pb2.Plan.from_bytes(plan_protobuf)
+
+    # TODO -- Read from the expected results from the corresponding .splan file.
+
     convert = SparkSubstraitConverter.SparkSubstraitConverter()
     substrait = convert.convert_plan(spark_plan)
     assertTrue(substrait)
