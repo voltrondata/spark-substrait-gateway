0f8a49a22b2fd42f61e2306effe30a7c11620635
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-02-26 01:07:57 -0800

    Fixed the tests.

--- src/gateway/converter/spark_to_substrait_test.py
+++ src/gateway/converter/spark_to_substrait_test.py
@@ -3,7 +3,8 @@ import pytest
 from pathlib import Path
 
 from gateway.converter.spark_to_substrait import SparkSubstraitConverter
-import spark.connect.base_pb2 as pb2
+from google.protobuf import text_format
+import spark.connect.base_pb2 as base_pb2
 
 
 test_case_directory = Path(os.path.dirname(os.path.realpath(__file__))) / 'data'
@@ -13,17 +14,18 @@ test_case_paths = [f for f in test_case_directory.iterdir() if f.name.endswith('
 test_case_names = [os.path.basename(p).removesuffix('.spark') for p in test_case_paths]
 
 
-@pytest.fixture(
-    params=test_case_paths,
+@pytest.mark.parametrize(
+    'path',
+    test_case_paths,
     ids=test_case_names,
 )
-def test_func(request: pytest.FixtureRequest):
-    with open(request.param) as f:
+def test_plan_conversion(path):
+    with open(path, "rb") as f:
         plan_protobuf = f.read()
-    spark_plan = pb2.Plan.from_bytes(plan_protobuf)
+    spark_plan = text_format.Parse(plan_protobuf, base_pb2.Plan())
 
     # TODO -- Read from the expected results from the corresponding .splan file.
 
-    convert = SparkSubstraitConverter.SparkSubstraitConverter()
+    convert = SparkSubstraitConverter()
     substrait = convert.convert_plan(spark_plan)
-    assertTrue(substrait)
+    assert substrait
