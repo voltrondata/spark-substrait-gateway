aa25b38f69b51942491246a1deab0b82f527bdb6
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-02-26 01:19:43 -0800

    Now read in the Substrait plan.

--- src/gateway/converter/spark_to_substrait_test.py
+++ src/gateway/converter/spark_to_substrait_test.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from gateway.converter.spark_to_substrait import SparkSubstraitConverter
 from google.protobuf import text_format
 import spark.connect.base_pb2 as base_pb2
+import substrait.gen.proto.plan_pb2 as plan_pb2
 
 
 test_case_directory = Path(os.path.dirname(os.path.realpath(__file__))) / 'data'
@@ -20,12 +21,17 @@ test_case_names = [os.path.basename(p).removesuffix('.spark') for p in test_case
     ids=test_case_names,
 )
 def test_plan_conversion(path):
+    # Read the Spark plan to convert.
     with open(path, "rb") as f:
-        plan_protobuf = f.read()
-    spark_plan = text_format.Parse(plan_protobuf, base_pb2.Plan())
+        plan_prototext = f.read()
+    spark_plan = text_format.Parse(plan_prototext, base_pb2.Plan())
 
-    # TODO -- Read from the expected results from the corresponding .splan file.
+    # The expected result is in the corresponding Substrait plan.
+    with open(path.with_suffix('.splan'), "rb") as f:
+        splan_prototext = f.read()
+    substrait_plan = text_format.Parse(splan_prototext, plan_pb2.Plan())
 
     convert = SparkSubstraitConverter()
     substrait = convert.convert_plan(spark_plan)
-    assert substrait
+
+    assert substrait == substrait_plan
