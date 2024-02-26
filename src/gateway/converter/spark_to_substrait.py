40faa0e88d4215c39d1756e32076c09a16aad59e
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-02-26 00:17:37 -0800

    Connect up the substrait-python protos.

--- src/gateway/converter/spark_to_substrait.py
+++ src/gateway/converter/spark_to_substrait.py
@@ -1,7 +1,8 @@
 import spark.connect.base_pb2 as pb2
+import substrait.gen.proto.plan_pb2 as plan_pb2
 
 
 class SparkSubstraitConverter:
 
-  def convert_plan(self, plan: pb2.Plan):
-    return ""
+  def convert_plan(self, plan: pb2.Plan) -> plan_pb2.Plan:
+    return plan_pb2.Plan()
