248a9e284367b62e3bd26b8e97edd171e9c04df5
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-02-26 21:15:43 -0800

    More lint fixes.

--- src/gateway/converter/spark_to_substrait_test.py
+++ src/gateway/converter/spark_to_substrait_test.py
@@ -1,12 +1,14 @@
 # SPDX-License-Identifier: Apache-2.0
+"""Tests for the Spark to Substrait plan conversion routines."""
 import os
-import pytest
 from pathlib import Path
 
-from gateway.converter.spark_to_substrait import SparkSubstraitConverter
 from google.protobuf import text_format
-import spark.connect.base_pb2 as base_pb2
-import substrait.gen.proto.plan_pb2 as plan_pb2
+import pytest
+from substrait.gen.proto import plan_pb2
+
+from gateway.converter.spark_to_substrait import SparkSubstraitConverter
+from spark.connect import base_pb2 as spark_base_pb2
 
 
 test_case_directory = Path(os.path.dirname(os.path.realpath(__file__))) / 'data'
@@ -22,10 +24,11 @@ test_case_names = [os.path.basename(p).removesuffix('.spark') for p in test_case
     ids=test_case_names,
 )
 def test_plan_conversion(request, path):
+    """Test the conversion of a Spark plan to a Substrait plan."""
     # Read the Spark plan to convert.
     with open(path, "rb") as f:
         plan_prototext = f.read()
-    spark_plan = text_format.Parse(plan_prototext, base_pb2.Plan())
+    spark_plan = text_format.Parse(plan_prototext, spark_base_pb2.Plan())
 
     # The expected result is in the corresponding Substrait plan.
     with open(path.with_suffix('.splan'), "rb") as f:
@@ -37,7 +40,7 @@ def test_plan_conversion(request, path):
 
     if request.config.getoption('rebuild_goldens'):
         if substrait != substrait_plan:
-            with open(path.with_suffix('.splan'), "wt") as f:
+            with open(path.with_suffix('.splan'), "wt", encoding='utf-8') as f:
                 f.write(text_format.MessageToString(substrait))
         return
 
