87090f18a6e4848ead170810cfb949614783f7f1
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-04-24 15:29:51 -0700

    feat:  improved arrow backend support (#58)
    - Adds workarounds to increase the likelihood that a function will be
      matched against the Acero engine.
    - Implements describe_table.
    - The tests are still not ready to be run continuously.

--- src/gateway/backends/arrow_backend.py
+++ src/gateway/backends/arrow_backend.py
@@ -6,18 +6,44 @@ import pyarrow as pa
 from substrait.gen.proto import plan_pb2
 
 from gateway.backends.backend import Backend
+from gateway.converter.rename_functions import RenameFunctionsForArrow
 
 
 class ArrowBackend(Backend):
     """Provides access to send Acero Substrait plans."""
 
+    _registered_tables: ClassVar[dict[str, Path]] = {}
+
+    def __init__(self, options):
+        """Initialize the Datafusion backend."""
+        super().__init__(options)
+        self._use_uri_workaround = options.use_arrow_uri_workaround
+
     # pylint: disable=import-outside-toplevel
     def execute(self, plan: plan_pb2.Plan) -> pa.lib.Table:
         """Execute the given Substrait plan against Acero."""
+        RenameFunctionsForArrow(use_uri_workaround=self._use_uri_workaround).visit_plan(plan)
+
         plan_data = plan.SerializeToString()
         reader = pa.substrait.run_query(plan_data)
         return reader.read_all()
 
     def register_table(self, name: str, path: Path, file_format: str = 'parquet') -> None:
         """Register the given table with the backend."""
-        raise NotImplementedError()
+        self._registered_tables[name] = path
+
+    def drop_table(self, name: str) -> None:
+        """Asks the backend to drop the given table."""
+        if self._registered_tables.get(name):
+            del self._registered_tables[name]
+
+    def describe_table(self, name: str):
+        """Return the schema of the given table."""
+        return pa.parquet.read_table(self._registered_tables[name]).schema
+
+    def _provide_tables(self, names: list[str], unused_schema) -> pyarrow.Table:
+        """Provide the tables requested."""
+        for name in names:
+            if name in self._registered_tables:
+                return pa.parquet.read_table(self._registered_tables[name])
+        raise ValueError(f'Table {names} not found in {self._registered_tables}')
