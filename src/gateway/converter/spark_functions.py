0ff7994d91a76982771cf8d2286591f3d5ddbeb2
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-06-20 22:45:22 -0700

    add missing mapping

--- src/gateway/converter/spark_functions.py
+++ src/gateway/converter/spark_functions.py
@@ -280,6 +280,10 @@ SPARK_SUBSTRAIT_MAPPING = {
         '/functions_string.yaml', 'char_length:str', type_pb2.Type(
             i64=type_pb2.Type.I64(
                 nullability=type_pb2.Type.Nullability.NULLABILITY_REQUIRED))),
+    'octet_length': ExtensionFunction(
+        '/functions_string.yaml', 'octet_length:str', type_pb2.Type(
+            i64=type_pb2.Type.I64(
+                nullability=type_pb2.Type.Nullability.NULLABILITY_REQUIRED))),
     'count': ExtensionFunction(
         '/functions_aggregate_generic.yaml', 'count:any', type_pb2.Type(
             i64=type_pb2.Type.I64(
