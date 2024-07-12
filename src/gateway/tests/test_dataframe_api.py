dfe6bc5d018cf248b83cb47f0ff0aeaaa78b23a0
David Sisson <EpsilonPrime@users.noreply.github.com>
2024-07-12 16:47:01 -0700

    Updated set of working tests.

--- src/gateway/tests/test_dataframe_api.py
+++ src/gateway/tests/test_dataframe_api.py
@@ -101,6 +101,8 @@ def mark_dataframe_tests_as_xfail(request):
         request.node.add_marker(pytest.mark.xfail(reason='duplicate name problem with joins'))
     if source == 'gateway-over-duckdb' and originalname == 'test_coalesce':
         request.node.add_marker(pytest.mark.xfail(reason='missing Substrait mapping'))
+    if source == 'gateway-over-datafusion' and originalname == 'test_coalesce':
+        request.node.add_marker(pytest.mark.xfail(reason='datafusion cast error'))
     if source == 'spark' and originalname == 'test_isnan':
         request.node.add_marker(pytest.mark.xfail(reason='None not preserved'))
     if source == 'gateway-over-datafusion' and originalname in [
