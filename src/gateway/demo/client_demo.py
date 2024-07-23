voltrondata/spark-substrait-gateway

commit 555495152ebc008018bc866fa47c9e158bb9d634

feat:  move TPC-H finding code out of the backend module (#47)

src/gateway/demo/client_demo.py
@@ -1,11 +1,23 @@
 # SPDX-License-Identifier: Apache-2.0
 """A PySpark client that can send sample queries to the gateway."""
+from pathlib import Path
 
 from backends.backend import Backend
 from pyspark.sql import DataFrame, SparkSession
 from pyspark.sql.functions import col
 
 USE_GATEWAY = True
+
+
+def find_tpch() -> Path:
+    """Find the location of the TPCH dataset."""
+    current_location = Path('').resolve()
+    while current_location != Path('/'):
+        location = current_location / 'third_party' / 'tpch' / 'parquet'
+        if location.exists():
+            return location.resolve()
+        current_location = current_location.parent
+    raise ValueError('TPCH dataset not found')
 
 
 # pylint: disable=fixme

src/backends/backend.py
@@ -68,35 +68,9 @@ class Backend:
         raise NotImplementedError()
 
     @staticmethod
-    def expand_location(location: Path | str) -> list[str]:
+    def _expand_location(location: Path | str) -> list[str]:
         """Expand the location of a file or directory into a list of files."""
-        # TODO -- Move into the gateway code.
         # TODO -- Handle more than just Parquet files.
         path = Path(location)
         files = Path(location).resolve().glob('*.parquet') if path.is_dir() else [path]
         return sorted(str(f) for f in files)
-
-    @staticmethod
-    def find_tpch() -> Path:
-        """Find the location of the TPCH dataset."""
-        # TODO -- Move into the gateway module.
-        current_location = Path('').resolve()
-        while current_location != Path('/'):
-            location = current_location / 'third_party' / 'tpch' / 'parquet'
-            if location.exists():
-                return location.resolve()
-            current_location = current_location.parent
-        raise ValueError('TPCH dataset not found')
-
-    def register_tpch(self):
-        """Register the entire TPC-H dataset."""
-        # TODO -- Remove all remaining uses and eliminate.
-        tpch_location = Backend.find_tpch()
-        self.register_table('customer', tpch_location / 'customer')
-        self.register_table('lineitem', tpch_location / 'lineitem')
-        self.register_table('nation', tpch_location / 'nation')
-        self.register_table('orders', tpch_location / 'orders')
-        self.register_table('part', tpch_location / 'part')
-        self.register_table('partsupp', tpch_location / 'partsupp')
-        self.register_table('region', tpch_location / 'region')
-        self.register_table('supplier', tpch_location / 'supplier')

src/backends/datafusion_backend.py
@@ -70,7 +70,7 @@ class DatafusionBackend(Backend):
             self, name: str, location: Path, file_format: str = 'parquet'
     ) -> None:
         """Register the given table with the backend."""
-        files = Backend.expand_location(location)
+        files = Backend._expand_location(location)
         if not files:
             raise ValueError(f"No parquet files found at {location}")
         # TODO: Add options to skip table registration if it already exists instead

src/backends/duckdb_backend.py
@@ -70,7 +70,7 @@ class DuckDBBackend(Backend):
             file_format: str = "parquet"
     ) -> None:
         """Register the given table with the backend."""
-        files = Backend.expand_location(location)
+        files = Backend._expand_location(location)
         if not files:
             raise ValueError(f"No parquet files found at {location}")
 
@@ -86,7 +86,7 @@ class DuckDBBackend(Backend):
         """Asks the backend to describe the given files."""
         files = paths
         if len(paths) == 1:
-            files = Backend.expand_location(paths[0])
+            files = Backend._expand_location(paths[0])
         # TODO -- Handle resolution of a combined schema.
         df = self._connection.read_parquet(files)
         schema = df.to_arrow_table().schema

commit 8e3df01465c56ff573e53514f31e3b3f077c2400

fix: update reference to find_tpch
Resolves bug introduced by 555495152ebc008018bc866fa47c9e158bb9d634.

src/gateway/demo/client_demo.py
@@ -23,7 +23,7 @@ def find_tpch() -> Path:
 # pylint: disable=fixme
 def get_customer_database(spark_session: SparkSession) -> DataFrame:
     """Register the TPC-H customer database."""
-    location_customer = str(Backend.find_tpch() / 'customer')
+    location_customer = str(find_tpch() / 'customer')
 
     return spark_session.read.parquet(location_customer, mergeSchema=False)
 