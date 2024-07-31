524b164e5ae4c33cf79663a856362a3b8023949c
prmoore77 <prmoore77@hotmail.com>
2024-07-31 12:08:50 -0400

    Linting fixes

--- src/gateway/security.py
+++ src/gateway/security.py
@@ -1,7 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
+import logging
+
 import grpc
 import jwt
-import logging
 
 
 class BearerTokenAuthInterceptor(grpc.ServerInterceptor):
@@ -10,11 +11,13 @@ class BearerTokenAuthInterceptor(grpc.ServerInterceptor):
                  secret_key: str,
                  logger: logging.Logger
                  ):
+        """Initializes the BearerTokenAuthInterceptor"""
         self.audience = audience
         self.secret_key = secret_key
         self.logger = logger
 
     def intercept_service(self, continuation, handler_call_details):
+        """Intercepts the incoming request and validates the bearer token"""
         # Extract metadata from the incoming request
         metadata = dict(handler_call_details.invocation_metadata)
         auth_header = metadata.get('authorization')
@@ -40,17 +43,17 @@ class BearerTokenAuthInterceptor(grpc.ServerInterceptor):
                 def unauthenticated_response(request, context):
                     context.set_code(grpc.StatusCode.UNAUTHENTICATED)
                     context.set_details('Token has expired')
-                    return None
+
                 return grpc.unary_unary_rpc_method_handler(unauthenticated_response)
             except jwt.exceptions.DecodeError:
                 def unauthenticated_response(request, context):
                     context.set_code(grpc.StatusCode.UNAUTHENTICATED)
                     context.set_details(f'Invalid token')
-                    return None
+
                 return grpc.unary_unary_rpc_method_handler(unauthenticated_response)
         else:
             def unauthenticated_response(request, context):
                 context.set_code(grpc.StatusCode.UNAUTHENTICATED)
                 context.set_details('No valid bearer token')
-                return None
+
             return grpc.unary_unary_rpc_method_handler(unauthenticated_response)
