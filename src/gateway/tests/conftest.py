f9388ffe8cd79759a307bd0d7dd6846131c50776
Richard Tia <richie.tia@gmail.com>
2024-08-01 19:23:50 -0700

    feat: add tpcds queries and spark tests (#62)
    Queries have been taken from the ibis project: 
    
    https://github.com/ibis-project/ibis/tree/main/ibis/backends/tests/tpc/queries/duckdb/ds
    
    Some queries have been modified to use backticks in the aliases instead
    of quotes
    in order to work with spark sql.
    
    Only 3 queries are able to run on duckdb currently. I've added markers
    to the other
    queries for now and will fix them in subsequent PRs.

--- src/gateway/tests/conftest.py
+++ src/gateway/tests/conftest.py
@@ -216,6 +216,7 @@ def register_tpcds_dataset(spark_session_for_setup: SparkSession) -> None:
     _register_table(spark_session_for_setup, benchmark, 'customer.parquet')
     _register_table(spark_session_for_setup, benchmark, 'customer_address.parquet')
     _register_table(spark_session_for_setup, benchmark, 'customer_demographics.parquet')
+    _register_table(spark_session_for_setup, benchmark, 'date_dim.parquet')
     _register_table(spark_session_for_setup, benchmark, 'household_demographics.parquet')
     _register_table(spark_session_for_setup, benchmark, 'income_band.parquet')
     _register_table(spark_session_for_setup, benchmark, 'inventory.parquet')
