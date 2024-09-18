97eabf70747e791f170729056599dcf49171e519
Richard Tia <richie.tia@gmail.com>
2024-09-17 20:56:54 -0700

    fix: ruff

--- src/gateway/converter/spark_to_substrait.py
+++ src/gateway/converter/spark_to_substrait.py
@@ -14,6 +14,11 @@ import pyspark.sql.connect.proto.base_pb2 as spark_pb2
 import pyspark.sql.connect.proto.expressions_pb2 as spark_exprs_pb2
 import pyspark.sql.connect.proto.relations_pb2 as spark_relations_pb2
 import pyspark.sql.connect.proto.types_pb2 as spark_types_pb2
+from google.protobuf.internal.wire_format import INT64_MAX
+from pyspark.sql.connect.proto import types_pb2
+from substrait.gen.proto import algebra_pb2, plan_pb2, type_pb2
+from substrait.gen.proto.extensions import extensions_pb2
+
 from gateway.converter.conversion_options import ConversionOptions
 from gateway.converter.spark_functions import (
     ExtensionFunction,
@@ -53,10 +58,6 @@ from gateway.converter.substrait_builder import (
     strlen,
 )
 from gateway.converter.symbol_table import SymbolTable
-from google.protobuf.internal.wire_format import INT64_MAX
-from pyspark.sql.connect.proto import types_pb2
-from substrait.gen.proto import algebra_pb2, plan_pb2, type_pb2
-from substrait.gen.proto.extensions import extensions_pb2
 
 
 class ExpressionProcessingMode(Enum):
